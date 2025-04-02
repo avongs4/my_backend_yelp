cdeab85ac4728641d935f2712b51c8975b6e1cd3
avongs <avongspharuna@gmail.com>
2025-04-02 15:12:58 +0100

    Setting up a react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RestaurantPage from "./pages/RestaurantPage";
+import LoginPage from "./pages/LoginPage";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/restaurant/:id" element={<RestaurantPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { Amplify } from "aws-amplify";
+// import { Authenticator } from "@aws-amplify/ui-react";
+// import awsconfig from "../aws-exports";
+
+// Amplify.configure(awsconfig);
+
+// function LoginPage() {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => (
+//         <div>
+//           <h1>Welcome, {user.username}</h1>
+//           <button onClick={signOut}>Sign Out</button>
+//         </div>
+//       )}
+//     </Authenticator>
+//   );
+// }
+
+// export default LoginPage;
