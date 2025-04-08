96470b27f62a5d6f6aace6c9a89f4320985153bd
avongs <avongspharuna@gmail.com>
2025-04-08 14:53:55 +0100

    Correcting restaurant category ongoing

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
